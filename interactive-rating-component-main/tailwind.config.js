b4890185b87794f17aa2c95ba006ef5cc770f7e9
getssh <gtamrat33@gmail.com>
2023-08-15 12:26:55 +0300

    Add overpass font family

--- interactive-rating-component-main/tailwind.config.js
+++ interactive-rating-component-main/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        overpass: ['Overpass', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
