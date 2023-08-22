07c70bcd008536783a75dc02cc59eadf6bfd4a8b
getssh <gtamrat33@gmail.com>
2023-08-23 02:05:46 +0300

    Add basic tructure to page

--- social-media-dashboard-with-theme-switcher-master/tailwind.config.js
+++ social-media-dashboard-with-theme-switcher-master/tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   darkMode: 'class',
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      primary: 'hsl(var(--color-primary) / <alpha-value>)',
+      secondary: 'hsl(var(--color-secondary) / <alpha-value>)',
+      facebook: 'hsl(var(--color-facebook) / <alpha-value>)',
+      twitter: 'hsl(var(--color-twitter) / <alpha-value>)',
+      instaone: 'hsl(var(--color-insta-one) / <alpha-value>)',
+      instatwo: 'hsl(var(--color-insta-two) / <alpha-value>)',
+      youtube: 'hsl(var(--color-youtube) / <alpha-value>)',
+    },
     extend: {
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
