d041b3bd286b39b2ac14991f24adb19f110636ed
getssh <gtamrat33@gmail.com>
2023-08-23 19:34:33 +0300

    Fix insta gradient border

--- social-media-dashboard-with-theme-switcher-master/tailwind.config.js
+++ social-media-dashboard-with-theme-switcher-master/tailwind.config.js
@@ -3,24 +3,21 @@ module.exports = {
   darkMode: 'class',
   content: ["./src/**/*.{html,js}"],
   theme: {
-    colors: {
-      primary: 'hsl(var(--color-primary) / <alpha-value>)',
-      secondary: 'hsl(var(--color-secondary) / <alpha-value>)',
-      facebook: 'hsl(var(--color-facebook) / <alpha-value>)',
-      twitter: 'hsl(var(--color-twitter) / <alpha-value>)',
-      instaone: 'hsl(var(--color-insta-one) / <alpha-value>)',
-      instatwo: 'hsl(var(--color-insta-two) / <alpha-value>)',
-      youtube: 'hsl(var(--color-youtube) / <alpha-value>)',
-      lightcardbg: 'hsl(var(--light-card-bg) / <alpha-value>)',
-      lighttextone: 'hsl(var(--light-text-one) / <alpha-value>)',
-      lighttexttwo: 'hsl(var(--light-text-two) / <alpha-value>)',
-    },
     extend: {
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
       },
-      backgroundImage: {
-        gradient: 'linear-gradient(to right, var(--color-insta-one), var(--color-insta-two))',
+      colors: {
+        primary: 'hsl(var(--color-primary) / <alpha-value>)',
+        secondary: 'hsl(var(--color-secondary) / <alpha-value>)',
+        facebook: 'hsl(var(--color-facebook) / <alpha-value>)',
+        twitter: 'hsl(var(--color-twitter) / <alpha-value>)',
+        instaone: 'hsl(var(--color-insta-one) / <alpha-value>)',
+        instatwo: 'hsl(var(--color-insta-two) / <alpha-value>)',
+        youtube: 'hsl(var(--color-youtube) / <alpha-value>)',
+        lightcardbg: 'hsl(var(--light-card-bg) / <alpha-value>)',
+        lighttextone: 'hsl(var(--light-text-one) / <alpha-value>)',
+        lighttexttwo: 'hsl(var(--light-text-two) / <alpha-value>)',
       },
     },
   },
