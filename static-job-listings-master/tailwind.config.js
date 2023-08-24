b0728173109c1deb20091f96d3d4e51759fc109a
getssh <gtamrat33@gmail.com>
2023-08-25 01:00:29 +0300

    Add basic strucure and index.js file

--- static-job-listings-master/tailwind.config.js
+++ static-job-listings-master/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
-  },
+    extend: {
+      fontFamily: {
+        league: ['League', 'sans-serif'],
+      },
+      colors: {
+        primary: 'hsl(var(--primary) / <alpha-value>)',
+        lighbg: 'hsl(var(--light-gray-bg) / <alpha-value>)',
+        lightfiler: 'hsl(var(--light-gray-filter) / <alpha-value>)',
+        darkgray: 'hsl(var(--dark-gray) / <alpha-value>)',
+        verydark: 'hsl(var(--very-dark-gray) / <alpha-value>)',
+      },
+    },
   plugins: [],
-}
+}}
