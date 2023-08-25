7ccefbc38a5a49937fafa9decb452d451c4a12f6
getssh <gtamrat33@gmail.com>
2023-08-25 13:46:14 +0300

    Add responisvness

--- static-job-listings-master/src/index.js
+++ static-job-listings-master/src/index.js
@@ -4,8 +4,8 @@ const filters = [];
 let allJobs = [];
 
 const filtersSection = document.createElement('div');
-filtersSection.className = 'filters-section';
-document.body.insertBefore(filtersSection, container);
+filtersSection.className = 'filters-section flex justify-start gap-2 bg-white py-3 px-4';
+// document.body.insertBefore(filtersSection, container);
 
 fetch('../data.json')
     .then(response => response.json())
@@ -20,29 +20,28 @@ fetch('../data.json')
 
 function renderFilters() {
   filtersSection.innerHTML = '';
+  filters.forEach(filter => {
+    const filterElement = document.createElement('div');
+    filterElement.className = 'filter';
+    filterElement.textContent = filter;
+
+    const deleteButton = document.createElement('span');
+    deleteButton.className = 'delete-button';
+    deleteButton.textContent = 'X';
+    deleteButton.addEventListener('click', () => removeFilter(filter));
+
+    filterElement.appendChild(deleteButton);
+    filtersSection.appendChild(filterElement);
+  });
 
   if (filters.length > 0) {
       const clearFiltersButton = document.createElement('button');
-      clearFiltersButton.className = 'clear-filters-button';
-      clearFiltersButton.textContent = 'Clear Filters';
+      clearFiltersButton.className = 'clear-filters-button self-end';
+      clearFiltersButton.textContent = 'Clear';
       clearFiltersButton.addEventListener('click', clearAllFilters);
       filtersSection.appendChild(clearFiltersButton);
   }
-
-  filters.forEach(filter => {
-      const filterElement = document.createElement('div');
-      filterElement.className = 'filter';
-      filterElement.textContent = filter;
-
-      const deleteButton = document.createElement('span');
-      deleteButton.className = 'delete-button';
-      deleteButton.textContent = 'X';
-      deleteButton.addEventListener('click', () => removeFilter(filter));
-
-      filterElement.appendChild(deleteButton);
-      filtersSection.appendChild(filterElement);
-  });
-  }
+}
 
 function renderJobs(jobs) {
     container.innerHTML = '';
@@ -50,12 +49,13 @@ function renderJobs(jobs) {
     const filteredJobs = filters.length > 0 ? filterJobsByFilters(jobs, filters) : jobs;
     renderFilters();
 
+    container.appendChild(filtersSection);
     filteredJobs.forEach((job) => {
         const jobContainer = document.createElement('div');
-        jobContainer.className = `flex justify-between items-center w-4/5 my-3 px-3 ${job.featured ? 'border-l-4 border-primary' : ''}`;
+        jobContainer.className = `flex flex-col sm:flex-row sm:flex-wrap lg:flex-nowrap justify-between items-center drop-shadow bg-white w-4/5 my-3 px-3 ${job.featured ? 'border-l-4 border-primary' : ''}`;
 
         const jobDetails = document.createElement('div');
-        jobDetails.className = 'flex items-center gap-5 p-2';
+        jobDetails.className = 'flex flex-col sm:flex-row items-center gap-5 p-2';
 
         const logoDiv = document.createElement('div');
         const logoImage = document.createElement('img');
@@ -67,11 +67,14 @@ function renderJobs(jobs) {
         const companyInfo = document.createElement('div');
         companyInfo.className = 'flex gap-2';
         const companyName = document.createElement('p');
+        companyName.className = 'text-primary font-bold';
         companyName.textContent = job.company;
         const isNew = document.createElement('p');
         isNew.textContent = job.new ? 'New!' : '';
+        isNew.className = job.new ? 'text-white bg-primary px-2 rounded-xl' : '';
         const isFeatured = document.createElement('p');
         isFeatured.textContent = job.featured ? 'Featured' : '';
+        isFeatured.className = job.featured ? 'text-white bg-verydark px-3 rounded-xl' : '';
         companyInfo.appendChild(companyName);
         companyInfo.appendChild(isNew);
         companyInfo.appendChild(isFeatured);
@@ -79,16 +82,20 @@ function renderJobs(jobs) {
 
         const position = document.createElement('h2');
         position.textContent = job.position;
+        position.className = 'font-bold cursor-pointer hover:text-primary'
         textDetails.appendChild(position);
 
         const contractDetails = document.createElement('div');
         contractDetails.className = 'flex gap-2';
         const postedAt = document.createElement('p');
         postedAt.textContent = job.postedAt;
+        postedAt.className = 'text-darkgray';
         const contractType = document.createElement('p');
         contractType.textContent = job.contract;
+        contractType.className = 'text-darkgray';
         const location = document.createElement('p');
         location.textContent = job.location;
+        location.className = 'text-darkgray';
         contractDetails.appendChild(postedAt);
         contractDetails.appendChild(contractType);
         contractDetails.appendChild(location);
@@ -98,20 +105,23 @@ function renderJobs(jobs) {
 
 
         const roleLanguagesTools = document.createElement('div');
-        roleLanguagesTools.className = 'flex gap-2';
+        roleLanguagesTools.className = 'flex flex-wrap sm:flex-nowrap gap-2 md:flex-nowrap';
         const roleElement = document.createElement('p');
         roleElement.textContent = job.role;
+        roleElement.className = 'bg-lightfiler px-3 text-primary font-medium cursor-pointer hover:bg-primary hover:text-white rounded-lg';
         roleElement.addEventListener('click', () => toggleFilter(job.role));
         roleLanguagesTools.appendChild(roleElement);
 
         const levelElement = document.createElement('p');
         levelElement.textContent = job.level;
+        levelElement.className = 'bg-lightfiler px-3 text-primary font-medium cursor-pointer hover:bg-primary hover:text-white rounded-lg';
         levelElement.addEventListener('click', () => toggleFilter(job.level));
         roleLanguagesTools.appendChild(levelElement);
 
         job.languages.forEach(language => {
           const languageElement = document.createElement('p');
           languageElement.textContent = language;
+          languageElement.className = 'bg-lightfiler px-3 text-primary font-medium cursor-pointer hover:bg-primary hover:text-white rounded-lg';
           languageElement.addEventListener('click', () => toggleFilter(language));
           roleLanguagesTools.appendChild(languageElement);
         });
@@ -119,6 +129,7 @@ function renderJobs(jobs) {
         job.tools.forEach(tool => {
           const toolElement = document.createElement('p');
           toolElement.textContent = tool;
+          toolElement.className = 'bg-lightfiler px-3 text-primary font-medium cursor-pointer hover:bg-primary hover:text-white rounded-lg';
           toolElement.addEventListener('click', () => toggleFilter(tool));
           roleLanguagesTools.appendChild(toolElement);
         });
@@ -132,7 +143,7 @@ function renderJobs(jobs) {
 
 function toggleFilter(filterValue) {
   if (filters.includes(filterValue)) {
-      filters.splice(filters.indexOf(filterValue), 1);
+    return;
   } else {
       filters.push(filterValue);
   }
