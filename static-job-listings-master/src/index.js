f31482356ecaebdded29852192b2977e1c451b65
getssh <gtamrat33@gmail.com>
2023-08-25 11:06:15 +0300

    Add js to create content daynamically

--- static-job-listings-master/src/index.js
+++ static-job-listings-master/src/index.js
@@ -1,2 +1,81 @@
-import '../data.json';
+const container = document.querySelector('.container');
+fetch('../data.json')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach((job) => {
+            const jobContainer = document.createElement('div');
+            jobContainer.className = `flex justify-between items-center w-4/5 my-3 px-3 ${job.featured ? 'border-l-4 border-primary' : ''}`;
 
+            const jobDetails = document.createElement('div');
+            jobDetails.className = 'flex items-center gap-5 p-2';
+
+            const logoDiv = document.createElement('div');
+            const logoImage = document.createElement('img');
+            logoImage.src = job.logo;
+            logoDiv.appendChild(logoImage);
+            jobDetails.appendChild(logoDiv);
+
+            const textDetails = document.createElement('div');
+            const companyInfo = document.createElement('div');
+            companyInfo.className = 'flex gap-2';
+            const companyName = document.createElement('p');
+            companyName.textContent = job.company;
+            const isNew = document.createElement('p');
+            isNew.textContent = job.new ? 'New!' : '';
+            const isFeatured = document.createElement('p');
+            isFeatured.textContent = job.featured ? 'Featured' : '';
+            companyInfo.appendChild(companyName);
+            companyInfo.appendChild(isNew);
+            companyInfo.appendChild(isFeatured);
+            textDetails.appendChild(companyInfo);
+
+            const position = document.createElement('h2');
+            position.textContent = job.position;
+            textDetails.appendChild(position);
+
+            const contractDetails = document.createElement('div');
+            contractDetails.className = 'flex gap-2';
+            const postedAt = document.createElement('p');
+            postedAt.textContent = job.postedAt;
+            const contractType = document.createElement('p');
+            contractType.textContent = job.contract;
+            const location = document.createElement('p');
+            location.textContent = job.location;
+            contractDetails.appendChild(postedAt);
+            contractDetails.appendChild(contractType);
+            contractDetails.appendChild(location);
+            textDetails.appendChild(contractDetails);
+
+            jobDetails.appendChild(textDetails);
+
+            const roleLanguagesTools = document.createElement('div');
+            roleLanguagesTools.className = 'flex gap-2';
+            const role = document.createElement('p');
+            role.textContent = job.role;
+            const level = document.createElement('p');
+            level.textContent = job.level;
+            roleLanguagesTools.appendChild(role);
+            roleLanguagesTools.appendChild(level);
+
+            job.languages.forEach(language => {
+                const languageElement = document.createElement('p');
+                languageElement.textContent = language;
+                roleLanguagesTools.appendChild(languageElement);
+            });
+
+            job.tools.forEach(tool => {
+                const toolElement = document.createElement('p');
+                toolElement.textContent = tool;
+                roleLanguagesTools.appendChild(toolElement);
+            });
+
+            jobContainer.appendChild(jobDetails);
+            jobContainer.appendChild(roleLanguagesTools);
+
+            container.appendChild(jobContainer);
+        });
+    })
+    .catch(error => {
+        console.error('Data loading error:', error);
+        container.innerHTML = 'Error couldnt load data';
+    });
